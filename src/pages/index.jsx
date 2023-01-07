6a377b57d4fc568c1690cbad735117c1857b98a1
islom <islommamatov2@gmail.com>
2023-01-07 12:34:45 +0500

    com

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Demos from "./demos/demos";
+import Home6Dark from "./homepage/home6-dark";
+
+const Home = () => {
+  return (
+    <>
+      <Home6Dark />
+    </>
+  );
+};
+
+export default Home;
